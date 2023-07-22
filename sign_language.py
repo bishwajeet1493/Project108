42b169530c71602a0a20c34b6e83522d81074003
bishwajeet1493 <82661881+bishwajeet1493@users.noreply.github.com>
2023-07-22 17:36:31 +0530

    Update sign_language.py

--- sign_language.py
+++ sign_language.py
@@ -26,26 +26,24 @@ while True:
                 lm_list.append(lm)
                 finger_fold_status = []
              #Code goes here   
-                for tip in finger_tips:
-                    x,y= int(lm_list[tip], x*w), int(lm_list[tip], y*h)
-                    cv2.circle(img, (x,y), 15, (255,0,0), cv2.FILLED)
+            for tip in finger_tips:
+                x,y= int(lm_list[tip], x*w), int(lm_list[tip], y*h)
+                cv2.circle(img, (x,y), 15, (255,0,0), cv2.FILLED)
                     
-                    if lm_list[tip].x < lm_list[tip - 3].x:
-                        cv2.circle(img, (x,y), 15, (0, 255, 0), cv2.FILLED)
-                        finger_fold_status.append(True)
-                    else:
-                        finger_fold_status.append(False)
-
-                        if all (finger_fold_status):
-                            #checking if the thumb is up
-                            if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_tip-2].y:
-                                print("LIKE")
-                                cv2.putText(img, "LIKE", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
-
-                            #check if thumb is down |
-                            if lm_list[thumb_tip].y > lm_list[thumb_tip-1].y > lm_list[thumb_tip-2].y:
-                                print("DISLIKE")
-                                cv2.putText(img,"DISLIKE", (20,30), cv2. FONT_HERSHEY_SIMPLEX, 1, (0,0,), 3)
+                if lm_list[tip].x < lm_list[tip - 3].x:                        
+                    cv2.circle(img, (x,y), 15, (0, 255, 0), cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
+                    if all (finger_fold_status):
+                        #checking if the thumb is up
+                        if lm_list[thumb_tip].y < lm_list[thumb_tip-1].y < lm_list[thumb_tip-2].y:
+                            print("LIKE")
+                            cv2.putText(img, "LIKE", (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
+                        #check if thumb is down |
+                        if lm_list[thumb_tip].y > lm_list[thumb_tip-1].y > lm_list[thumb_tip-2].y:
+                            print("DISLIKE")
+                            cv2.putText(img,"DISLIKE", (20,30), cv2. FONT_HERSHEY_SIMPLEX, 1, (0,0,), 3)
 
             mp_drawing.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_drawing.DrawingSpec((0,0,255),2,2),
